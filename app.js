03762dd6712463746960c57c1c16c5cf06091cb5
ACitronella <phuriwat.13042@gmail.com>
2022-03-22 01:09:35 +0700

    route only api

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const search = require("./search.js");
 // const home = require("./home.js");
 const user_api = require("./user.js");
 const music_api = require("./music.js");
+const playlist_api = require("./playlist.js");
 require('dotenv').config();
 
 const app = express();
@@ -14,10 +15,11 @@ const app = express();
 // app.use("/public", express.static(path.join(__dirname, "templetes", "public")));
 // app.use("/images", express.static(path.join(__dirname, "templetes", "images")));
 // app.use("/", home.home_route);
-app.use("/login", login.login_route);
-app.use("/search", search.search_api_route);
+// app.use("/login", login.login_route);
+// app.use("/search", search.search_api_route);
 app.use("/api/user", user_api.user_api_route);
 app.use("/api/music", music_api.music_api_route);
+app.use("/api/playlist", playlist_api.playlist_api_route);
 
 
 app.listen(process.env.PORT, function () {
