86be43849f25d312c135e9000fa207708e5bd20a
ACitronella <phuriwat.13042@gmail.com>
2022-03-21 23:06:30 +0700

    update db

--- Project_Phrase2.sql
+++ Project_Phrase2.sql
@@ -3,7 +3,7 @@ CREATE DATABASE IF NOT EXISTS `Project_Phrase2`;
 USE `Project_Phrase2`;
 
 CREATE TABLE User (
-	UserID			int		PRIMARY KEY AUTO_INCREMENT,
+    UserID			int		    PRIMARY KEY AUTO_INCREMENT,
     UserName		varchar(20),
     FirstName		varchar(100),
     LastName		varchar(100),
@@ -13,74 +13,73 @@ CREATE TABLE User (
     Role			int,
     Email			varchar(100),
     Password		varchar(64),
-    Salt			varchar(40)
+    Salt			varchar(40),
+    IsDeleted       boolean
 );
 
 CREATE TABLE Music (
-	MusicID			int		PRIMARY KEY AUTO_INCREMENT,
+    MusicID			int		PRIMARY KEY AUTO_INCREMENT,
     UserID			int,
     MusicName		varchar(250),
     MusicIMG		varchar(256),
     MusicFile		varchar(256),
     TimeCreated		datetime,
-    CONSTRAINT fk_UserID FOREIGN KEY (UserID)
-	REFERENCES User(UserID)
+    IsDeleted       boolean,
+
+    CONSTRAINT fk_UserID FOREIGN KEY (UserID) 
+    REFERENCES User(UserID)
 );
 
 CREATE TABLE Playlist (
-	PlaylistID		int		PRIMARY KEY AUTO_INCREMENT,
+    PlaylistID		int		PRIMARY KEY AUTO_INCREMENT,
     PlaylistCreator	int,
     PlaylistName	varchar(250),
     PlaylistIMG		varchar(256),
+    TimeCreated     datetime,
+    IsDeleted       boolean,
+
     CONSTRAINT fk_PlaylistCreator FOREIGN KEY (PlaylistCreator)
-	REFERENCES User(UserID)
+    REFERENCES User(UserID)
 );
 
 CREATE TABLE LoginLog (
-	UserID			int,
+    UserID			int,
     LoginTime		datetime,
+
     CONSTRAINT fk_UserID2 FOREIGN KEY (UserID)
-	REFERENCES User(UserID)
+    REFERENCES User(UserID)
 );
 
 CREATE TABLE UserFollowUser (
-	FolloweeID			int,
+    FolloweeID			int,
     FollowerID			int,
     FollowTime			datetime,
-    CONSTRAINT fk_FolloweeID FOREIGN KEY (FolloweeID)
-	REFERENCES User(UserID),
-    CONSTRAINT fk_FollowerID FOREIGN KEY (FollowerID)
-	REFERENCES User(UserID)
-);
+    IsUnFollow          boolean,
 
-CREATE TABLE UserCreateMusic (
-	UserID			int,
-    MusicID			int,
-    CreateTime		datetime,
-    IsDeleted		boolean,
-    CONSTRAINT fk_UserID3 FOREIGN KEY (UserID)
-	REFERENCES User(UserID),
-    CONSTRAINT fk_MusicID FOREIGN KEY (MusicID)
-	REFERENCES Music(MusicID)
+    CONSTRAINT fk_FolloweeID FOREIGN KEY (FolloweeID)
+    REFERENCES User(UserID),
+    CONSTRAINT fk_FollowerID FOREIGN KEY (FollowerID)
+    REFERENCES User(UserID)
 );
 
 CREATE TABLE UserFollowPlaylist (
-	UserID			int,
-    PlaylistID			int,
+    UserID			int,
+    PlaylistID		int,
     FollowTime		datetime,
+    IsUnfollow      boolean,
+    
     CONSTRAINT fk_UserID4 FOREIGN KEY (UserID)
-	REFERENCES User(UserID),
+    REFERENCES User(UserID),
     CONSTRAINT fk_PlaylistID FOREIGN KEY (PlaylistID)
-	REFERENCES Playlist(PlaylistID)
+    REFERENCES Playlist(PlaylistID)
 );
 
-CREATE TABLE UserCreatePlaylist (
-	UserID			int,
-    PlaylistID		int,
-    CreateTime		datetime,
-    IsDeleted		boolean,
-    CONSTRAINT fk_UserID5 FOREIGN KEY (UserID)
-	REFERENCES User(UserID),
-    CONSTRAINT fk_PlaylistID2 FOREIGN KEY (PlaylistID)
-	REFERENCES Playlist(PlaylistID)
+CREATE TABLE MusicInPlaylist(
+    MusicID     int,
+    PlaylistID  int,
+
+    CONSTRAINT fk_musicinplaylist_musicid FOREIGN KEY (MusicID)
+    REFERENCES Music(MusicID),
+    CONSTRAINT fk_musicinplaylist_playlistid FOREIGN KEY (PlaylistID)
+    REFERENCES Playlist(PlaylistID)
 );
--- test_db.js
+++ test_db.js
@@ -14,33 +14,33 @@ const connection = mysql.createConnection({
 connection.connect();
 
 // connection.query(`
-// DROP TABLE LoginLog;
+// DROP TABLE IF EXISTS LoginLog;
 // `);
 // connection.query(`
-// DROP TABLE UserFollowUser;
+// DROP TABLE IF EXISTS UserFollowUser;
 // `);
 // connection.query(`
-// DROP TABLE UserCreateMusic;
+// DROP TABLE IF EXISTS UserCreateMusic;
 // `);
 // connection.query(`
-// DROP TABLE UserCreatePlaylist;
+// DROP TABLE IF EXISTS UserCreatePlaylist;
 // `);
 // connection.query(`
-// DROP TABLE UserFollowPlaylist;
+// DROP TABLE IF EXISTS UserFollowPlaylist;
 // `);
 // connection.query(`
-// DROP TABLE Playlist;
+// DROP TABLE IF EXISTS Playlist;
 // `);
 // connection.query(`
-// DROP TABLE Music;
+// DROP TABLE IF EXISTS Music;
 // `);
 // connection.query(`
-// DROP TABLE User;
+// DROP TABLE IF EXISTS User;
 // `);
 
-// connection.query(
-// `CREATE TABLE User (
-//     UserID			int		PRIMARY KEY AUTO_INCREMENT,
+// connection.query(`
+// CREATE TABLE User (
+//     UserID			int		    PRIMARY KEY AUTO_INCREMENT,
 //     UserName		varchar(20),
 //     FirstName		varchar(100),
 //     LastName		varchar(100),
@@ -50,86 +50,93 @@ connection.connect();
 //     Role			int,
 //     Email			varchar(100),
 //     Password		varchar(64),
-//     Salt			varchar(40)
+//     Salt			varchar(40),
+//     IsDeleted       boolean
 // );
 // `);
 
-// connection.query( 
-// `CREATE TABLE Music (
-//         MusicID			int		PRIMARY KEY AUTO_INCREMENT,
-//         UserID			int,
-//         MusicName		varchar(250),
-//         MusicIMG		varchar(256),
-//         MusicFile		varchar(256),
-//         TimeCreated		datetime,
-//         CONSTRAINT fk_UserID FOREIGN KEY (UserID) REFERENCES User(UserID)
-//     );
+// connection.query(`
+// CREATE TABLE Music (
+//     MusicID			int		PRIMARY KEY AUTO_INCREMENT,
+//     UserID			int,
+//     MusicName		varchar(250),
+//     MusicIMG		varchar(256),
+//     MusicFile		varchar(256),
+//     TimeCreated		datetime,
+//     IsDeleted       boolean,
+
+//     CONSTRAINT fk_UserID FOREIGN KEY (UserID) 
+//     REFERENCES User(UserID)
+// );
 // `);
 
-// connection.query(
-// `CREATE TABLE Playlist (
+// connection.query(`
+// CREATE TABLE Playlist (
 //     PlaylistID		int		PRIMARY KEY AUTO_INCREMENT,
 //     PlaylistCreator	int,
 //     PlaylistName	varchar(250),
 //     PlaylistIMG		varchar(256),
+//     TimeCreated     datetime,
+//     IsDeleted       boolean,
+
 //     CONSTRAINT fk_PlaylistCreator FOREIGN KEY (PlaylistCreator)
 //     REFERENCES User(UserID)
-// );`);
+// );
+// `);
 
-// connection.query(
-// `CREATE TABLE LoginLog (
+// connection.query(`
+// CREATE TABLE LoginLog (
 //     UserID			int,
 //     LoginTime		datetime,
+
 //     CONSTRAINT fk_UserID2 FOREIGN KEY (UserID)
 //     REFERENCES User(UserID)
-// );`);
+// );
+// `);
 
-// connection.query(
-// `CREATE TABLE UserFollowUser (
+
+// connection.query(`
+// CREATE TABLE UserFollowUser (
 //     FolloweeID			int,
 //     FollowerID			int,
 //     FollowTime			datetime,
+//     IsUnFollow          boolean,
+
 //     CONSTRAINT fk_FolloweeID FOREIGN KEY (FolloweeID)
 //     REFERENCES User(UserID),
 //     CONSTRAINT fk_FollowerID FOREIGN KEY (FollowerID)
 //     REFERENCES User(UserID)
-// );`
 // );
+// `);
 
-// connection.query(
-// `CREATE TABLE UserCreateMusic (
+// connection.query(  // add isunfollow? 
+// `
+// CREATE TABLE UserFollowPlaylist (
 //     UserID			int,
-//     MusicID			int,
-//     CreateTime		datetime,
-//     IsDeleted		boolean,
-//     CONSTRAINT fk_UserID3 FOREIGN KEY (UserID)
-//     REFERENCES User(UserID),
-//     CONSTRAINT fk_MusicID FOREIGN KEY (MusicID)
-//     REFERENCES Music(MusicID)
-// );`);
-
-// connection.query( 
-// `CREATE TABLE UserFollowPlaylist (
-//     UserID			int,
-//     PlaylistID			int,
+//     PlaylistID		int,
 //     FollowTime		datetime,
+//     IsUnfollow      boolean,
+    
 //     CONSTRAINT fk_UserID4 FOREIGN KEY (UserID)
 //     REFERENCES User(UserID),
 //     CONSTRAINT fk_PlaylistID FOREIGN KEY (PlaylistID)
 //     REFERENCES Playlist(PlaylistID)
-// );`);
+// );
+// `);
 
-// connection.query( 
-// `CREATE TABLE UserCreatePlaylist (
-//     UserID			int,
-//     PlaylistID		int,
-//     CreateTime		datetime,
-//     IsDeleted		boolean,
-//     CONSTRAINT fk_UserID5 FOREIGN KEY (UserID)
-//     REFERENCES User(UserID),
-//     CONSTRAINT fk_PlaylistID2 FOREIGN KEY (PlaylistID)
+// connection.query(/* Represent music itself in the playlist. M <-> M relation */`
+// CREATE TABLE MusicInPlaylist(
+//     MusicID     int,
+//     PlaylistID  int,
+
+//     CONSTRAINT fk_musicinplaylist_musicid FOREIGN KEY (MusicID)
+//     REFERENCES Music(MusicID),
+//     CONSTRAINT fk_musicinplaylist_playlistid FOREIGN KEY (PlaylistID)
 //     REFERENCES Playlist(PlaylistID)
-// );`);
+// );
+// `);
+
+// // new table about
 
 connection.query("SELECT * FROM User;", function(error, results, fields) {
     if(error) throw error;
@@ -146,6 +153,12 @@ connection.query("SELECT * FROM LoginLog;", function(error, results, fields) {
 connection.query("SELECT * FROM Music;", function(error, results, fields){
     if(error) throw error;
     console.log("Music: ");
+    console.log(results);
+});
+
+connection.query("SELECT * FROM UserFollowUser;",  function(error, results, fields){
+    if(error) throw error;
+    console.log("UserFollowUser: ");
     console.log(results);
 });
 
