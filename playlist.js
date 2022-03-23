71246d461a9c2a069bf35a211113b3ed8490b3d6
ACitronella <phuriwat.13042@gmail.com>
2022-03-24 00:08:42 +0700

    add playlist.search by authorname and playlist.edit

--- playlist.js
+++ playlist.js
@@ -27,7 +27,7 @@ playlist_api_route.post("/create", function(req, res){
     let record = {
         "PlaylistCreator" : playlist.PlaylistCreator,
         "PlaylistName" : playlist.PlaylistName,
-        "PlaylistIMG" : playlist.PlaylistImg,
+        "PlaylistIMG" : playlist.PlaylistIMG ? playlist.PlaylistIMG : "",
         "TimeCreated" : new Date().toISOString().slice(0, 19).replace('T', ' '),
         "IsDeleted" : false
     };
@@ -37,17 +37,38 @@ playlist_api_route.post("/create", function(req, res){
     });
 });
 
-playlist_api_route.get("/search_by_playlistname", function(req, res){
+playlist_api_route.put("/edit", function(req, res){
+    let playlist = req.body.Playlist;
+    let playlist_id = playlist.PlaylistID;
+    delete playlist.TimeCreated; // strictly cant edit
+    delete playlist.IsDeleted; // strictly cant edit with this method
+
+    connection.query("UPDATE Playlist SET ? WHERE PlaylistID = ?;", [playlist, playlist_id], function(error, results, fields){
+        if(error) res.status(500).send({error: true, message: error.toString()});
+        else res.send({error: false, message: "update playlist complete"});
+    });
+});
+
+playlist_api_route.get("/search_by_authorname/:AuthorName", function(req, res){
+    let author_name = req.params.AuthorName;
+    let author_name_query = "%" + author_name + "%";
+    connection.query("SELECT PlaylistID, PlaylistName, PlaylistIMG, u.UserName FROM Playlist p INNER JOIN User u ON p.PlaylistCreator = u.UserID WHERE u.UserName LIKE ? AND p.IsDeleted = false AND u.IsDeleted = false;", author_name_query, function(error, results, fields){
+        if(error) res.status(500).send({error: true, message: error.toString(), playlists: null});
+        else res.send({error: false, message: "found playlists", playlists: results});
+    });
+});
+
+playlist_api_route.get("/search_by_playlistname/:PlaylistName", function(req, res){
     /**
      * 
      * {
      *      "PlaylistName" : value
      * }
      */
-    let playlist_name = req.query.PlaylistName; // str
+    let playlist_name = req.params.PlaylistName; // str
     if(!playlist_name) {res.status(400).send({error: true, message: "playlist name can't be null"}); return;}
     let playlist_name_query = "%" + playlist_name + "%";
-    connection.query("SELECT * FROM Playlist WHERE PlaylistName LIKE ?;", playlist_name_query, function(error, results, fields){
+    connection.query("SELECT * FROM Playlist WHERE PlaylistName LIKE ? AND WHERE IsDeleted = False;", playlist_name_query, function(error, results, fields){
         if(error) res.status(500).send({error: true, message: error.toString()});
         else res.send({error: false, playlists:results, message: "playlist(s) found"});
     });
@@ -68,6 +89,17 @@ playlist_api_route.post("/user_follow", function(req, res){
         else res.send({error: false, message: "Follow playlist complete"});
     });
 });
+
+playlist_api_route.delete("/delete", function(req, res){
+    let playlist_id = req.body.PlaylistID;
+    connection.query("UPDATE Playlist SET IsDeleted = true WHERE PlaylistID = ?;", playlist_id, function(error, results, fields){
+        if(error) res.status(500).send({error: true, message: error.toString()});
+        else if(results.affectedRows == 0) res.send({error: false, message: "playlist not found"});
+        else if(results.affectedRows > 0 && results.changedRows == 0) res.send({error: false, message: "playlist is already deleted"});
+        else res.send({error: false, message: "playlist delete complete"}); 
+    });
+});
+
 
 playlist_api_route.delete("/user_follow", function(req, res){
     let user_id = req.body.UserID; // int
