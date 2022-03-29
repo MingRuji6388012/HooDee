6d2fccc01bf98ae8aaf8a090bcc08a776dfd805f
ACitronella <phuriwat.13042@gmail.com>
2022-03-30 03:00:09 +0700

    on search handling

--- templetes/js/search_bar.js
+++ templetes/js/search_bar.js
@@ -4,3 +4,41 @@ function on_click_cross(id){
     console.log(elem.value);
     elem.value = "";
 }
+
+async function on_search(){
+    // e.preventDefault(); // for f*cking jquery
+    const query_text = document.getElementById("search-bar").value;
+    if(!query_text.length){
+        console.log("please fill something into the text field");
+        return;
+    }
+    const all_quatifier = document.getElementById("all-radio-button").checked || false;
+    const user_quatifier = document.getElementById("user-radio-button").checked || false;
+    const music_quatifier = document.getElementById("music-radio-button").checked || false;
+    const playlist_quatifier = document.getElementById("playlist-radio-button").checked || false;
+
+    let user_list = null, music_list = null, playlist_list = null; 
+    if(all_quatifier || user_quatifier){
+        user_list = fetch(`/api/user/search_by_username?UserName=${query_text}`, {
+            method: "GET",
+        })
+    }
+    if(all_quatifier || music_quatifier){
+        console.log(query_text);
+        console.log(`/api/music/search_by_musicname/${query_text}`);
+        music_list = fetch(`/api/music/search_by_musicname/${query_text}`, {
+            method: "GET",
+        })
+    }
+    if(all_quatifier || playlist_quatifier){
+        playlist_list = fetch(`/api/playlist/search_by_playlistname/${query_text}`, {
+            method: "GET",
+        })
+    }
+    let data = {
+        user: await (user_list.then(res => res.json())),
+        music: await (music_list.then(res => res.json())),
+        playlist: await (playlist_list.then(res => res.json()))
+    };
+    console.log(data);
+}
