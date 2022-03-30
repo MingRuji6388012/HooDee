9d862fe3311851e8ac2fcbbafeb3f6189c82c599
ACitronella <phuriwat.13042@gmail.com>
2022-03-30 23:17:27 +0700

    routing for search/result page

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require("path");
 // const login = require("./login.js");
 const search = require("./search.js");
 // const home = require("./home.js");
+const result = require("./result.js");
+
 const user_api = require("./user.js");
 const music_api = require("./music.js");
 const playlist_api = require("./playlist.js");
@@ -19,7 +21,9 @@ app.use("/public", express.static(path.join(__dirname, "templetes", "public")));
 app.use("/images", express.static(path.join(__dirname, "templetes", "images")));
 
 // app.use("/login", login.login_route);
-app.use("/search", search.search_api_route);
+app.use("/search", search.search_route);
+app.use("/result", result.result_route);
+
 app.use("/api/user", user_api.user_api_route);
 app.use("/api/music", music_api.music_api_route);
 app.use("/api/playlist", playlist_api.playlist_api_route);
