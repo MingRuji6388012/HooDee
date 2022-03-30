1819c3173f1011bf11e918279fc8e38ea8cb7556
ACitronella <phuriwat.13042@gmail.com>
2022-03-30 23:17:54 +0700

    routing for search/result page

--- result.js
+++ result.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const path = require("path");
-const connection_import = require("./database.js");
+// const connection_import = require("./database.js");
 // const connection = connection_import.connection;
 require('dotenv').config();
 
@@ -10,8 +10,10 @@ result_route.use(express.json());
 result_route.use(express.urlencoded({ extended: true }));
 
 result_route.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "templetes", "html", "search.html"));
+    res.sendFile(path.join(__dirname, "templetes", "html", "result.html"));
 });
 
 // result_route.get("/")
+
+module.exports.result_route = result_route;
 
