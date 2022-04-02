87c66b087b333ebbcbac44d6a4336d81737a15eb
Rujiphart Charatvaraphan <81845323+MingRuji6388012@users.noreply.github.com>
2022-04-02 15:39:04 +0700

    Update UI Alignment on search tab on search.html and text-decoration on common.css

--- fetch.js
+++ fetch.js
@@ -476,12 +476,3 @@ const fetch = require("node-fetch");
 //     }
 // )
 
-fetch("http://localhost:3000/api/user_follow",
-    {
-        method: "POST",
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({
-            ""
-        })
-    }
-)
