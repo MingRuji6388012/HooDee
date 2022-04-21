13a6fdf751db22738911b00f810be5c7a45a61a3
ACitronella <phuriwat.13042@gmail.com>
2022-04-21 17:45:12 +0700

    fix click bug in halftopcard

--- frontend/src/component/HalfTopCard.tsx
+++ frontend/src/component/HalfTopCard.tsx
@@ -14,24 +14,24 @@ interface HalfTopCardProps{
 class HalfTopCard extends Component<HalfTopCardProps> {
     render(): ReactNode {
         return (
-            <a href={this.props.href}>
+            <div>
                 <div className="card music-card top-card">
                     <div className="row no-gutters fluid">
                         <div className="col-md-6">
                             <img src={this.props.img_url} className="img-fluid rounded-start card-img-top"/>
                         </div>
-                        <div className="col-md-5">
+                        <a className="col-md-5" href={this.props.href}>
                             <div className="card-body">
                                 <figcaption className="card-title h6">{this.props.top_text}</figcaption>
                                 <figcaption className="card-text">{this.props.bottom_text}</figcaption>
                             </div>
-                        </div>
+                        </a>
                         <div className="col-md-1">
                             <Dropdown type={this.props.type} dropdownOn={this.props.card_info}/> 
                         </div>
                     </div>
                 </div>
-            </a> 
+            </div> 
         );
     }
 }
