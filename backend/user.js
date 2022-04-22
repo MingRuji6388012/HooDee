ace496a1dd720a2e4e4554da77638cd8d4da71ed
ACitronella <phuriwat.13042@gmail.com>
2022-04-23 01:51:37 +0700

    update edit user

--- backend/user.js
+++ backend/user.js
@@ -242,7 +242,8 @@ user_api_route.put("/edit", function(req, res){
      *          "UserName" : value or null,
      *          "FirstName" : value or null,
      *          "LastName" : value or null,
-     *          "DOB" : value or null
+     *          "DOB" : value or null,
+     *          "Password" : value or null
      *      }
      * }
      * in req body
@@ -254,15 +255,30 @@ user_api_route.put("/edit", function(req, res){
      * }
      * 
      */
-    console.log("editing");
-    let user = req.body.User
-    let user_id = user.UserID;
-    if(user.Password !== undefined){ delete user.password; } // strictly cant change
+    console.log("editing user ", user.UserID);
+    const user = req.body.User
+    const user_id = user.UserID;
     if(user.Email !== undefined){ delete user.Email; }   // strictly cant change
+    if(user.Salt !== undefined){ delete user.Salt; }     // strictly cant change
+    if(user.TimeCreated !== undefined){ delete user.TimeCreated; }     // strictly cant change
+    if(user.IsDeleted !== undefined){ delete user.IsDeleted; }     // strictly cant change
+    if(user.Secret !== undefined){ delete user.Secret; }     // strictly cant change
+    if(user.Role !== undefined){ delete user.Role; }     // strictly cant change
+    
+    if(user.Password === null){ delete user.password; }
     if(user.UserName === null){ delete user.UserName; }
     if(user.FirstName === null){ delete user.FirstName; }
     if(user.LastName === null){ delete user.LastName; }
     if(user.DOB === null){ delete user.DOB; }
+
+    if(user.Password){
+        const password = req.body.User.Password;
+        const salt = crypto.randomBytes(20).toString("hex");
+        const hashed_password = crypto.createHash('sha256').update(password + salt).digest('hex');
+        user.Password = hashed_password;
+        user.Salt = salt;
+    }
+
     connection.query("UPDATE User SET ? WHERE UserID = ?;", [user, user_id], function (error, results, fields){
         if(error) res.status(500).send({error: true, message: error.toString()});
         else res.send({error: false, message: "edit user profile complete"});
