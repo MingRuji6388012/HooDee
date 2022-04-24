699283d7fa578f465f38eb5d5c395e1ff3e32c9d
ACitronella <phuriwat.13042@gmail.com>
2022-04-24 16:43:04 +0700

    restrict somepart of code depends on roles

--- frontend/src/page/add.tsx
+++ frontend/src/page/add.tsx
@@ -1,9 +1,9 @@
 import { Component } from "react";
-import { ROLES } from "../common";
+import { goHomeKiddos, ROLES } from "../common";
 import AddMusicComponent from "../component/AddMusic";
 import AddPlaylistComponent from "../component/AddPlaylist";
-import AddUserComponent from "../component/AddUser";
 import "../css/DomainSelection.css";
+import { UserButInSessionStorage } from "../model/User";
 import SignUpPage from "./signup";
 
 interface AddPageState {
@@ -13,10 +13,16 @@ interface AddPageState {
     userPressed: boolean;
     musicPressed: boolean;
     playlistPressed: boolean;
+    userInSession: UserButInSessionStorage;
 }
 class AddPage extends Component<{}, AddPageState>{
     constructor(props:any){
         super(props)
+
+        const userJSON = sessionStorage.getItem("user");
+        if(!userJSON) {goHomeKiddos(); return;} // not in session, can't use this page
+        const user = JSON.parse(userJSON) as UserButInSessionStorage;
+
         this.state = {
             classNameUser: 'DomainButton',
             classNameMusic: 'DomainButton',
@@ -24,8 +30,9 @@ class AddPage extends Component<{}, AddPageState>{
             userPressed: false,
             musicPressed: false,
             playlistPressed: false,
+            userInSession: user
         }
-        this.changeClassNameUser = this.changeClassNameUser.bind(this);
+        this.changeClassNameUser = this.changeClassNameUser.bind(this); 
         this.changeClassNameMusic = this.changeClassNameMusic.bind(this);
         this.changeClassNamePlaylist = this.changeClassNamePlaylist.bind(this);
     }
@@ -68,18 +75,16 @@ class AddPage extends Component<{}, AddPageState>{
         return(
             <div>
                 <div className="DomainList">
-                    <button onClick={this.changeClassNameUser} className={this.state.classNameUser}>User/Artist/Admin</button>
-                    <button onClick={this.changeClassNameMusic} className={this.state.classNameMusic}>Music</button>
+                    {(this.state.userInSession.Role === ROLES.admin) && <button onClick={this.changeClassNameUser} className={this.state.classNameUser}>User/Artist/Admin</button>}
+                    {(this.state.userInSession.Role !== ROLES.user) && <button onClick={this.changeClassNameMusic} className={this.state.classNameMusic}>Music</button>}
                     <button onClick={this.changeClassNamePlaylist} className={this.state.classNamePlaylist}>Playlist</button>
                 </div>
 
-                {this.state.userPressed && <SignUpPage email="" firstname="" lastname="" password="" role={ROLES.user} username="" />}
-                {this.state.musicPressed && <AddMusicComponent musicName="" musicURL="" />}
+                {(this.state.userInSession.Role === ROLES.admin) && this.state.userPressed && <SignUpPage email="" firstname="" lastname="" password="" role={ROLES.user} username="" />}
+                {(this.state.userInSession.Role !== ROLES.user) && this.state.musicPressed && <AddMusicComponent musicName="" musicURL="" />}
                 {this.state.playlistPressed && <AddPlaylistComponent playlistName="" />}
             </div>
-
-
-        )
+        );
     }
 }
 
